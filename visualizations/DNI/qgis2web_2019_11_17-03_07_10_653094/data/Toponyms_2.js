57e9a7858e5a11bcc7edb125b424dc000c104530
Harel Dan <harel.dunn@gmail.com>
2019-11-17 03:08:39 +0200

    update
    update

---
+++ visualizations/DNI/qgis2web_2019_11_17-03_07_10_653094/data/Toponyms_2.js
@@ -0,0 +1,22 @@
+var json_Toponyms_2 = {
+"type": "FeatureCollection",
+"name": "Toponyms_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "label": "קרית יערים" }, "geometry": { "type": "Point", "coordinates": [ 35.121542439626012, 31.818789133394667 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "הר יערים" }, "geometry": { "type": "Point", "coordinates": [ 35.199274041553871, 31.825791452705786 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "מכמש" }, "geometry": { "type": "Point", "coordinates": [ 35.274079249593477, 31.876713713324094 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "אילנה" }, "geometry": { "type": "Point", "coordinates": [ 35.019486791445999, 31.850599195413846 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "גבעת בנימין" }, "geometry": { "type": "Point", "coordinates": [ 35.183052613819093, 31.849146649276637 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "שכם" }, "geometry": { "type": "Point", "coordinates": [ 35.274089375869409, 32.21817803485839 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "בית אל" }, "geometry": { "type": "Point", "coordinates": [ 35.219345259828884, 31.937078680271508 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "עפרה" }, "geometry": { "type": "Point", "coordinates": [ 35.273906341569429, 31.976375526056383 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "אלון צעננים" }, "geometry": { "type": "Point", "coordinates": [ 35.55093985876821, 33.093115968528728 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "אלון תבור" }, "geometry": { "type": "Point", "coordinates": [ 35.377430790297815, 32.693624396300329 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "אלון מורה" }, "geometry": { "type": "Point", "coordinates": [ 35.274920705907334, 32.20805861696266 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "אלוני ממרא" }, "geometry": { "type": "Point", "coordinates": [ 35.118308060184532, 31.535300143085269 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "בית לחם" }, "geometry": { "type": "Point", "coordinates": [ 35.209449313915059, 31.709719083911025 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "ירושלים" }, "geometry": { "type": "Point", "coordinates": [ 35.221310532359126, 31.779330995742061 ] } },
+{ "type": "Feature", "properties": { "id": null, "label": "חברון" }, "geometry": { "type": "Point", "coordinates": [ 35.115398325577161, 31.524587173524981 ] } }
+]
+}
